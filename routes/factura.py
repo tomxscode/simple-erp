6f7070eef7e3497c2e5fecb715507466f12c4465
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-11-29 23:50:31 -0300

    Mostrar factura

--- routes/factura.py
+++ routes/factura.py
@@ -1,9 +1,10 @@
 import datetime
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template
 from models import db
 from models.Factura import Factura, DetalleFactura
 from models.Empresa import Empresa
 from models.Usuario import Usuario
+from utils.general import convertir_fecha, dinero_formato
 
 factura = Blueprint('factura', __name__)
 
@@ -37,3 +38,17 @@ def factura_api():
     db.session.add(factura)
     db.session.commit()
     return jsonify({'success': True, 'message': 'Factura creada correctamente', 'factura': factura.id}), 200
+  
+
+@factura.route('/factura/<int:id>')
+def ver_factura(id):
+  factura = Factura.query.get(id)
+  detalles = DetalleFactura.query.filter_by(factura_id=id).all()
+  
+  def obtener_nombre_empresa(empresa_id):
+    empresa = Empresa.query.get(empresa_id)
+    if empresa:
+      return empresa.nombre
+    return None
+  
+  return render_template('factura/factura.html', factura=factura, detalles=detalles, obtener_nombre_empresa=obtener_nombre_empresa, convertir_fecha=convertir_fecha, dinero_formato=dinero_formato)
